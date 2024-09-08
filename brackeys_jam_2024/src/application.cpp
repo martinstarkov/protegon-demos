b801f76fadf0f1ac1af0077f18ea8769e2429e1d
Martin <emailregisterlogin@gmail.com>
2024-09-08 18:30:23 +0200

    Basic car

--- brackeys_jam_2024/src/application.cpp
+++ brackeys_jam_2024/src/application.cpp
@@ -5,13 +5,131 @@ using namespace ptgn;
 constexpr const V2_int resolution{ 960, 540 };
 constexpr const V2_int center{ resolution / 2 };
 
+struct Transform {
+	V2_float position;
+	float rotation{ 0.0f };
+};
+
+struct Size {
+	V2_float size;
+};
+
+struct RigidBody {
+	V2_float velocity;
+	V2_float acceleration;
+	V2_float max_velocity;
+};
+
+struct VehicleComponent {
+	float forward_thrust{ 0.0f };
+	float backward_thrust{ 0.0f };
+	float turn_speed{ 0.0f };
+};
+
 class GameScene : public Scene {
 public:
+	ecs::Manager manager;
+
+	ecs::Entity player;
+
 	GameScene() {}
 
-	void Init() final {}
+	void Init() final {
+		CreatePlayer();
+	}
 
-	void Update(float dt) final {}
+	void Update(float dt) final {
+		PlayerInput(dt);
+
+		PlayerPhysics(dt);
+
+		Draw();
+	}
+
+	void Draw() {
+		DrawPlayer();
+	}
+
+	// Init functions.
+
+	void CreatePlayer() {
+		player = manager.CreateEntity();
+
+		player.Add<Texture>(Texture{ "resources/entity/car.png" });
+
+		auto& transform	   = player.Add<Transform>();
+		transform.position = center;
+
+		player.Add<Size>(V2_float{ 16, 30 });
+
+		auto& rigid_body		= player.Add<RigidBody>();
+		rigid_body.max_velocity = { 500.0f, 500.0f };
+
+		auto& vehicle			= player.Add<VehicleComponent>();
+		vehicle.forward_thrust	= 300.0f;
+		vehicle.backward_thrust = 100.0f;
+		vehicle.turn_speed		= 10.0f;
+	}
+
+	// Update functions.
+
+	void PlayerInput(float dt) {
+		PTGN_ASSERT(player.Has<RigidBody>());
+		PTGN_ASSERT(player.Has<VehicleComponent>());
+		PTGN_ASSERT(player.Has<Transform>());
+
+		auto& rigid_body = player.Get<RigidBody>();
+		auto& vehicle	 = player.Get<VehicleComponent>();
+		auto& transform	 = player.Get<Transform>();
+
+		V2_float unit_direction{ V2_float{ 1.0f, 0.0f }.Rotated(transform.rotation) };
+
+		V2_float thrust;
+
+		if (game.input.KeyDown(Key::W)) {
+			thrust = unit_direction * vehicle.forward_thrust;
+		} else if (game.input.KeyDown(Key::S)) {
+			thrust = unit_direction * vehicle.backward_thrust;
+		}
+
+		if (game.input.KeyDown(Key::A)) {
+			transform.rotation += vehicle.turn_speed * dt;
+		}
+		if (game.input.KeyDown(Key::D)) {
+			transform.rotation -= vehicle.turn_speed * dt;
+		}
+
+		rigid_body.acceleration += thrust;
+	}
+
+	void PlayerPhysics(float dt) {
+		PTGN_ASSERT(player.Has<RigidBody>());
+		PTGN_ASSERT(player.Has<Transform>());
+
+		auto& rigid_body = player.Get<RigidBody>();
+		auto& transform	 = player.Get<Transform>();
+
+		rigid_body.velocity += rigid_body.acceleration * dt;
+
+		transform.position += rigid_body.velocity * dt;
+
+		rigid_body.acceleration = {};
+	}
+
+	// Draw functions.
+
+	void DrawPlayer() {
+		PTGN_ASSERT(player.Has<Transform>());
+		PTGN_ASSERT(player.Has<Texture>());
+		PTGN_ASSERT(player.Has<Size>());
+
+		auto& player_transform{ player.Get<Transform>() };
+
+		game.renderer.DrawTexture(
+			player.Get<Texture>(), player_transform.position, player.Get<Size>().size, {}, {},
+			Origin::Center, Flip::None, player_transform.rotation
+		);
+	}
 };
 
 struct TextButton {
