ad71f53912f46f24a6a9b91159e270c235d9b454
Martin Starkov <emailregisterlogin@gmail.com>
2024-10-24 15:02:25 +0300

    Begin adding movement component

--- scream_jam_2024/src/application.cpp
+++ scream_jam_2024/src/application.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "components/collider.h"
+#include "components/movement.h"
 #include "components/sprite.h"
 #include "components/transform.h"
 #include "core/game.h"
@@ -14,6 +15,7 @@
 #include "math/geometry/polygon.h"
 #include "math/math.h"
 #include "math/vector2.h"
+#include "physics/physics.h"
 #include "protegon/protegon.h"
 #include "renderer/color.h"
 #include "renderer/origin.h"
@@ -39,8 +41,8 @@ public:
 	ecs::Entity platform1;
 	ecs::Entity boss1_entity;
 
-	float gravity{ 1000000.0f };
-	V2_float player_acceleration{ 500000.0f, 1800000.0f };
+	float gravity{ 10000.0f };
+	V2_float player_acceleration{ 500000.0f, 18000000.0f };
 
 	GameScene() {}
 
@@ -78,34 +80,35 @@ public:
 		ecs::Entity entity = manager.CreateEntity();
 
 		entity.Add<Transform>(resolution / 2.0f + V2_float{ 100, 100 });
-		auto& rb		= entity.Add<RigidBody>();
-		rb.gravity		= gravity;
-		rb.max_velocity = 1000.0f;
-		rb.drag			= 15.0f;
+		auto& rb				 = entity.Add<RigidBody>();
+		rb.gravity				 = 1.0f;
+		auto& m					 = entity.Add<Movement>();
+		m.data.run_max_speed	 = 9;
+		m.data.run_acceleration	 = 13;
+		m.data.run_decceleration = 16;
+
+		rb.drag						  = 0.22f;
+		m.data.jump_force			  = 13;
+		m.data.jump_cut_gravity		  = 0.4f;
+		m.data.coyote_time			  = 0.15f;
+		m.data.jump_input_buffer_time = 0.1f;
+		m.data.fall_gravity			  = 2.0f;
 
 		auto& cg = entity.Add<ColliderGroup>(entity, manager);
 		cg.AddBox(
 			  "body", { 70 - 0 * 0, 88 }, 0, { 55, 129 }, Origin::Center, true,
 			  [](ecs::Entity e1, ecs::Entity e2) {
-				  PTGN_LOG("overlap started between ", e1.GetId(), " and ", e2.GetId());
+				  PTGN_LOG("collision started between ", e1.GetId(), " and ", e2.GetId());
 			  },
 			  [](ecs::Entity e1, ecs::Entity e2) {
-				  PTGN_LOG("overlap between ", e1.GetId(), " and ", e2.GetId());
-				  PTGN_ASSERT(e1.Has<BoxCollider>());
-				  auto& b1 = e1.Get<BoxCollider>();
-				  PTGN_ASSERT(b1.parent.IsAlive());
-				  PTGN_ASSERT(b1.parent.Has<Transform>());
-				  PTGN_ASSERT(b1.parent.Has<RigidBody>());
-				  PTGN_ASSERT(e2.Has<BoxCollider>());
-				  auto& b2			= e2.Get<BoxCollider>();
-				  auto intersection = b1.GetAbsoluteRect().Intersects(b2.GetAbsoluteRect());
-				  b1.parent.Get<Transform>().position	+= intersection.normal * intersection.depth;
-				  b1.parent.Get<RigidBody>().velocity.y	 = 0;
+				  PTGN_LOG("collision between ", e1.GetId(), " and ", e2.GetId());
 			  },
 			  [](ecs::Entity e1, ecs::Entity e2) {
-				  PTGN_LOG("overlap stopped between ", e1.GetId(), " and ", e2.GetId());
-			  }
-		).Add<DrawColor>(color::Purple);
+				  PTGN_LOG("collision stopped between ", e1.GetId(), " and ", e2.GetId());
+			  },
+			  nullptr, false, true
+		)
+			.Add<DrawColor>(color::Purple);
 		/*cg.AddBox("head0", { 74 - 0 * 0, 27 }, 0, { 144, 14 }).Add<DrawColor>(color::Green);
 		cg.AddBox("head1", { 211 - 150 * 1, 27 }, 0, { 116, 13 }).Add<DrawColor>(color::Green);
 		cg.AddBox("head2", { 352 - 150 * 2, 28 }, 0, { 103, 11 }).Add<DrawColor>(color::Green);
@@ -142,8 +145,8 @@ public:
 		PTGN_ASSERT(player.Has<RigidBody>());
 		auto& player_rb = player.Get<RigidBody>();
 
-		player_rb.acceleration = {};
-		if (game.input.KeyPressed(Key::W)) {
+		/*player_rb.acceleration = {};
+		if (game.input.KeyDown(Key::W)) {
 			player_rb.acceleration.y = -player_acceleration.y * dt;
 		}
 		if (game.input.KeyPressed(Key::S)) {
@@ -154,12 +157,17 @@ public:
 		}
 		if (game.input.KeyPressed(Key::D)) {
 			player_rb.acceleration.x = player_acceleration.x * dt;
+		}*/
+		for (auto [e, t, rb, m] : manager.EntitiesWith<Transform, RigidBody, Movement>()) {
+			m.Update(t, rb);
 		}
 		for (auto [e, t, rb] : manager.EntitiesWith<Transform, RigidBody>()) {
 			rb.Update();
-			t.position += rb.velocity * game.dt();
 		}
 		game.collision.Update(manager);
+		for (auto [e, t, rb] : manager.EntitiesWith<Transform, RigidBody>()) {
+			t.position += rb.velocity * game.physics.dt();
+		}
 		Draw();
 	}
 
