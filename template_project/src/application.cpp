29b9d0d00bdf20852ab1facda3985edb7e75108d
Martin Starkov <emailregisterlogin@gmail.com>
2025-01-23 13:23:11 +0200

    Updated template project

--- template_project/src/application.cpp
+++ template_project/src/application.cpp
@@ -2,18 +2,29 @@
 
 using namespace ptgn;
 
-class SetupScene : public Scene {
+constexpr V2_int window_size{ 1280, 720 };
+constexpr Color window_color{ color::Transparent };
+constexpr const char* window_title{ "Sample Title" };
+
+class SampleScene : public Scene {
 public:
-	SetupScene() {
-		game.window.SetSize({ 960, 540 });
-		game.window.SetTitle("Game");
-		game.draw.SetClearColor(color::Black);
+	Texture texture{ "resources/sample.png" };
+
+	void Enter() override {
+		PTGN_LOG("Entered sample scene");
 	}
 
-	void Update() override {}
+	void Update() override {
+		texture.Draw({ game.window.GetCenter(), {}, Origin::Center });
+	}
+
+	void Exit() override {
+		PTGN_LOG("Exited sample scene");
+	}
 };
 
 int main() {
-	game.Start<SetupScene>();
+	game.Init(window_title, window_size, window_color);
+	game.scene.Enter<SampleScene>("sample");
 	return 0;
 }
