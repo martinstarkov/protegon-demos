a02972bd7d54cfd0f26153d9551fca376ae3b3c6
Martin Starkov <emailregisterlogin@gmail.com>
2025-02-17 04:43:31 +0200

    Added lights and colliders

--- brackeys_jam_2025_1/src/application.cpp
+++ brackeys_jam_2025_1/src/application.cpp
@@ -10,7 +10,6 @@ constexpr float zoom{ 3.0f };
 
 struct CarController {
 	float acceleration{ 0.0f };
-	float max_speed{ 0.0f };
 	float drag{ 0.0f };
 	float steer_angle{ 0.0f };
 	float traction{ 0.0f };
@@ -31,7 +30,8 @@ struct CarController {
 		return drifting_;
 	}
 
-	void Update(Transform& transform) {
+	// @param dt Unit: seconds.
+	void Update(Transform& transform, RigidBody& rb, float dt) {
 		V2_float dir;
 
 		bool forward{ game.input.KeyPressed(forward_key) };
@@ -50,19 +50,18 @@ struct CarController {
 			dir.y = 1.0f;
 		}
 
-		float steer_input{ dir.y };
 		float forward_input{ dir.x };
+		float steer_input{ dir.x * dir.y };
 
-		auto forward_vector = [&](const Transform& t) {
-			return V2_float{ forward_input, 0.0f }.Rotated(t.rotation);
+		auto forward_vector = [&](float rotation) {
+			return V2_float{ forward_input, 0.0f }.Rotated(rotation);
 		};
 
-		velocity_		   += forward_vector(transform) * acceleration * game.dt();
-		transform.position += velocity_ * game.dt();
+		rb.AddAcceleration(forward_vector(transform.rotation) * acceleration, dt);
 
 		float steer{ drifting_ ? drifting_steer_angle : steer_angle };
 
-		transform.rotation += steer_input * velocity_.Magnitude() * steer * game.dt();
+		transform.rotation += steer_input * rb.velocity.Magnitude() * steer * dt;
 
 		transform.rotation = ClampAngle2Pi(transform.rotation);
 
@@ -71,33 +70,26 @@ struct CarController {
 				", forward vector: ", forward_vector(transform)
 			);*/
 
-		float d{ drifting_ ? drifting_drag : drag };
-
-		velocity_ *= 1.0f / (1.0f + d * game.dt());
-		velocity_  = Clamp(velocity_, -max_speed, max_speed);
+		rb.drag = drifting_ ? drifting_drag : drag;
 
 		float trac{ drifting_ ? drifting_traction : traction };
 
-		auto norm_vel{ velocity_.Normalized() };
-		auto forward_vec{ forward_vector(transform) };
+		auto norm_vel{ rb.velocity.Normalized() };
+		auto forward_vec{ forward_vector(transform.rotation) };
 
 		auto current_drift_angle{ forward_vec.Angle(norm_vel) };
 
 		drifting_ = current_drift_angle >= DegToRad(10.0f);
 
-		velocity_ = velocity_.Magnitude() * Lerp(norm_vel, forward_vec, trac * game.dt());
-
-		// PTGN_LOG("velocity: ", velocity_);
+		rb.velocity = rb.velocity.Magnitude() * Lerp(norm_vel, forward_vec, trac * dt);
 	}
 
 private:
 	bool drifting_{ false };
-	V2_float velocity_;
 };
 
 void to_json(json& j, const CarController& c) {
 	j = json{ { "acceleration", c.acceleration },
-			  { "max_speed", c.max_speed },
 			  { "drag", c.drag },
 			  { "steer_angle", c.steer_angle },
 			  { "traction", c.traction },
@@ -111,11 +103,6 @@ void from_json(const json& j, CarController& c) {
 		j.at("acceleration").get_to(c.acceleration);
 	} else {
 		c.acceleration = 0.0f;
-	}
-	if (j.contains("max_speed")) {
-		j.at("max_speed").get_to(c.max_speed);
-	} else {
-		c.max_speed = 0.0f;
 	}
 	if (j.contains("drag")) {
 		j.at("drag").get_to(c.drag);
@@ -149,11 +136,22 @@ void from_json(const json& j, CarController& c) {
 	}
 }
 
+ecs::Entity CreateLight(ecs::Manager& manager, const PointLight& light) {
+	auto entity{ manager.CreateEntity() };
+	entity.Add<PointLight>(light);
+	entity.Add<Visible>();
+	return entity;
+}
+
 ecs::Entity CreateCar(ecs::Manager& manager, const path& car_json_filepath) {
-	auto entity{ CreateSprite(manager, "car") };
+	std::string_view key{ "car" };
+	auto entity{ CreateSprite(manager, key) };
 	auto j{ LoadJson(car_json_filepath) };
 	auto& transform{ entity.Add<Transform>(j.at("Transform")) };
+	auto rb_json{ j.at("RigidBody") };
+	auto& rb{ entity.Add<RigidBody>(rb_json) };
 	auto& controller{ entity.Add<CarController>(j.at("CarController")) };
+	auto& box{ entity.Add<BoxCollider>(entity, game.texture.GetSize(key) * 0.75f, Origin::Center) };
 	return entity;
 }
 
@@ -165,9 +163,11 @@ void CreateRoad(ecs::Manager& manager, const V2_int& top_left) {
 }
 
 void CreateBuilding(ecs::Manager& manager, const V2_int& top_left) {
-	auto entity = CreateSprite(manager, "building");
+	std::string_view key{ "building" };
+	auto entity = CreateSprite(manager, key);
 	entity.Add<Transform>(top_left);
 	entity.Add<Origin>(Origin::TopLeft);
+	auto& box = entity.Add<BoxCollider>(entity, game.texture.GetSize(key), Origin::TopLeft);
 }
 
 void CreateSkidmark(ecs::Manager& manager, const Transform& car_transform) {
@@ -201,6 +201,8 @@ void CreateLevel(ecs::Manager& manager, const path& filepath) {
 class GameScene : public Scene {
 public:
 	ecs::Entity car;
+	ecs::Entity red_light;
+	ecs::Entity blue_light;
 
 	void Enter() override {
 		camera.primary.SetZoom(zoom);
@@ -216,17 +218,65 @@ public:
 
 		car = CreateCar(manager, "resources/json/car.json");
 
+		PointLight light1;
+
+		float light_radius{ 150.0f };
+
+		light1.SetRadius(light_radius)
+			.SetIntensity(1.0f)
+			.SetFalloff(1.0f)
+			.SetColor(color::Red)
+			.SetAmbientColor(color::LightRed);
+		PointLight light2{ light1 };
+		light2.SetColor(color::Blue).SetAmbientColor(color::LightBlue);
+
+		red_light = CreateLight(manager, light1);
+		red_light.Add<Transform>();
+
+		milliseconds fade_time{ 250 };
+		float max_ambient_intensity{ 0.3f };
+
+		red_light.Add<Tween>()
+			.During(fade_time)
+			.Yoyo()
+			.Repeat(-1)
+			.OnUpdate([=](float f) mutable {
+				red_light.Get<PointLight>().SetRadius(light_radius * f);
+				red_light.Get<PointLight>().SetAmbientIntensity(max_ambient_intensity * f);
+			})
+			.Start();
+		blue_light = CreateLight(manager, light2);
+		blue_light.Add<Transform>();
+		blue_light.Add<Tween>()
+			.During(fade_time)
+			.Reverse()
+			.Repeat(-1)
+			.Yoyo()
+			.OnUpdate([=](float f) mutable {
+				blue_light.Get<PointLight>().SetRadius(light_radius * f);
+				blue_light.Get<PointLight>().SetAmbientIntensity(max_ambient_intensity * f);
+			})
+			.Start();
+
 		camera.primary.StartFollow(car);
 	}
 
 	void Update() override {
-		auto [texture_key, transform, controller] = car.Get<TextureKey, Transform, CarController>();
+		auto [texture_key, transform, rb, controller, box] =
+			car.Get<TextureKey, Transform, RigidBody, CarController, BoxCollider>();
+		V2_float light_offset{ V2_float{ 1.0f, 0.0f }.Rotated(transform.rotation) *
+							   game.texture.GetSize(texture_key).y / 2.0f };
+		blue_light.Get<Transform>().position =
+			camera.primary.TransformToScreen(transform.position + light_offset);
+		red_light.Get<Transform>().position =
+			camera.primary.TransformToScreen(transform.position + light_offset);
 		/*if (game.input.KeyPressed(Key::SPACE)) {
 			controller.SetDrifting(true);
 		} else {
 			controller.SetDrifting(false);
 		}*/
-		controller.Update(transform);
+		controller.Update(transform, rb, physics.dt());
+		// box.rotation = transform.rotation;
 		if (controller.IsDrifting()) {
 			CreateSkidmark(manager, transform);
 			texture_key = "car_drift";
