a1d0fbecaedb21836d034fa4acad513034415775
Martin <emailregisterlogin@gmail.com>
2025-05-09 18:02:26 +0200

    Added y based sorting

--- cozy_spring_jam_2025/src/application.cpp
+++ cozy_spring_jam_2025/src/application.cpp
@@ -1,34 +1,40 @@
+#include <functional>
+
 #include "audio/audio.h"
+#include "components/common.h"
 #include "components/draw.h"
 #include "components/generic.h"
 #include "components/movement.h"
+#include "components/transform.h"
 #include "core/entity.h"
 #include "core/game.h"
+#include "core/game_object.h"
 #include "core/manager.h"
-#include "core/transform.h"
-#include "math/collision/collider.h"
+#include "core/resource_manager.h"
+#include "core/time.h"
+#include "core/tween.h"
 #include "math/noise.h"
 #include "math/vector2.h"
+#include "physics/collision/collider.h"
 #include "physics/rigid_body.h"
 #include "protegon/protegon.h"
-#include "renderer/color.h"
-#include "renderer/origin.h"
+#include "rendering/api/color.h"
+#include "rendering/api/origin.h"
 #include "scene/camera.h"
 #include "scene/scene.h"
 #include "scene/scene_manager.h"
 #include "ui/button.h"
-#include "utility/time.h"
 
 using namespace ptgn;
 
 constexpr V2_int window_size{ 1280, 720 };
 constexpr float camera_zoom{ 4.0f };
-constexpr Color color1{ 206, 79, 25, 255 };
+constexpr Color bg_color{ 126, 206, 120, 255 };
 
 constexpr int walk_volume{ 30 };
 constexpr int repair_volume{ 30 };
 constexpr int music_volume{ 50 };
-constexpr int wind_volume{ 20 };
+constexpr int wind_volume{ 5 };
 constexpr std::size_t walk_sound_frequency{ 2 }; // every second repeat of the walk animation
 
 constexpr CollisionCategory player_category{ 0 };
@@ -38,22 +44,15 @@ struct WalkRepeats : public ArithmeticComponent<int> {
 	using ArithmeticComponent::ArithmeticComponent;
 };
 
-class GameScene : public Scene {
-public:
-	FractalNoise fractal_noise;
+struct Player : public GameObject {
+	Player() = default;
 
-	GameScene() {
-		LoadResources("resources/data/resources.json");
-	}
-
-	Entity CreatePlayer() {
-		auto entity = manager.CreateEntity();
-
+	Player(Manager& manager) : GameObject{ manager } {
 		V2_float player_starting_position{ 0.0f, 0.0f };
 
-		entity.Add<Transform>(player_starting_position);
-		auto& rb = entity.Add<RigidBody>();
-		entity.Add<Enabled>();
+		Add<Transform>(player_starting_position);
+		auto& rb = Add<RigidBody>();
+		Add<Enabled>();
 
 		V2_float hitbox_size{ 10, 6 };
 		V2_float hitbox_offset{ 0, 8 };
@@ -66,10 +65,10 @@ public:
 		interaction_hitbox.Add<BoxCollider>(V2_float{ 28, 28 }, Origin::Center);
 		interaction_hitbox.Add<Transform>(V2_float{});
 
-		entity.AddChild("body", body_hitbox);
-		entity.AddChild("interaction", interaction_hitbox);
+		AddChild("body", body_hitbox);
+		AddChild("interaction", interaction_hitbox);
 
-		auto& movement = entity.Add<TopDownMovement>();
+		auto& movement = Add<TopDownMovement>();
 
 		// Maximum movement speed.
 		movement.max_speed = 0.7f * 60.0f;
@@ -83,33 +82,31 @@ public:
 		movement.friction = 1.0f;
 
 		V2_uint animation_count{ 4, 3 };
-		V2_int player_size		= { 12, 24 };
+		V2_int player_size		= { 16, 17 };
 		V2_float animation_size = player_size;
 		milliseconds animation_duration{ 1000 };
 
-		entity.Add<WalkRepeats>();
+		Add<WalkRepeats>();
 
-		auto& anim_map = entity.Add<AnimationMap>(
+		auto& anim_map = Add<AnimationMap>(
 			"down",
-			CreateAnimation(
-				manager, "player_anim", animation_count.x, animation_size, animation_duration
-			)
+			Animation(manager, "player_anim", animation_count.x, animation_size, animation_duration)
 		);
 		auto& a0 = anim_map.GetActive();
 		auto& a1 = anim_map.Load(
-			"right", CreateAnimation(
+			"right", Animation(
 						 manager, "player_anim", animation_count.x, animation_size,
 						 animation_duration, V2_float{ 0, animation_size.y }
 					 )
 		);
 		auto& a2 = anim_map.Load(
-			"up", CreateAnimation(
+			"up", Animation(
 					  manager, "player_anim", animation_count.x, animation_size, animation_duration,
 					  V2_float{ 0, 2.0f * animation_size.y }
 				  )
 		);
 
-		auto on_repeat = [=]() {
+		auto on_repeat = [entity = GetEntity()]() {
 			auto& repeats{ entity.Get<WalkRepeats>() };
 			++repeats.GetValue();
 			bool repeat{ repeats % walk_sound_frequency == 0 };
@@ -119,18 +116,18 @@ public:
 			game.sound.Play("walk");
 		};
 
-		a0.SetParent(entity);
-		a1.SetParent(entity);
-		a2.SetParent(entity);
+		a0.SetParent(*this);
+		a1.SetParent(*this);
+		a2.SetParent(*this);
 
 		a0.Add<callback::AnimationRepeat>(on_repeat);
 		a1.Add<callback::AnimationRepeat>(on_repeat);
 		a2.Add<callback::AnimationRepeat>(on_repeat);
 
-		movement.on_move_start = [=]() {
+		movement.on_move_start = [entity = GetEntity()]() {
 			entity.Get<AnimationMap>().GetActive().Get<Tween>().Start(false);
 		};
-		movement.on_direction_change = [=](MoveDirection) {
+		movement.on_direction_change = [entity = GetEntity()](MoveDirection) {
 			auto& a{ entity.Get<AnimationMap>() };
 			auto dir{ entity.Get<TopDownMovement>().GetDirection() };
 			auto& prev_active{ a.GetActive() };
@@ -153,14 +150,34 @@ public:
 			auto& current_active{ a.GetActive() };
 			current_active.Get<Tween>().Start(false);
 		};
-		movement.on_move_stop = [=]() {
+		movement.on_move_stop = [entity = GetEntity()]() {
 			entity.Get<AnimationMap>().GetActive().Get<Tween>().Reset();
 		};
+	}
+};
 
-		return entity;
+struct Flower : public Sprite {
+	Flower() = default;
+
+	Flower(Manager& manager, const V2_float& position, std::string_view texture_key) :
+		Sprite{ manager, texture_key } {
+		SetPosition(position);
+	}
+};
+
+class GameScene : public Scene {
+public:
+	FractalNoise fractal_noise;
+
+	GameScene() {
+		LoadResources("resources/data/resources.json");
 	}
 
-	Entity player;
+	Player player;
+	Flower f1;
+	Flower f2;
+	Flower f3;
+	Flower f4;
 
 	void Enter() override {
 		fractal_noise.SetOctaves(2);
@@ -168,12 +185,15 @@ public:
 		fractal_noise.SetLacunarity(5);
 		fractal_noise.SetPersistence(3);
 
-		player = CreatePlayer();
+		player = Player{ manager };
+
+		f1 = Flower{ manager, { 30, 30 }, "flower_0" };
+		f2 = Flower{ manager, { 37, 30 }, "flower_0" };
+		f3 = Flower{ manager, { 30, 35 }, "flower_0" };
+		f4 = Flower{ manager, { 37, 35 }, "flower_0" };
 
 		camera.primary.SetZoom(camera_zoom);
-
-		auto ship1 = CreateSprite(manager, "ship");
-		ship1.Add<Transform>();
+		camera.primary.StartFollow(player);
 
 		game.sound.SetVolume("wind", wind_volume);
 		game.sound.Play("wind", 0);
@@ -182,14 +202,13 @@ public:
 	}
 
 	void Update() override {
-		auto& player_pos = player.Get<Transform>().position;
-
-		if (player.Get<TopDownMovement>().keys_enabled) {
-			camera.primary.SetPosition(player_pos);
-		}
+		// DrawDebugCircle({ f1.GetPosition().x, f1.GetLowestY() }, 1.0f, color::Orange, -1.0f);
+		// DrawDebugCircle({ player.GetPosition().x, player.GetLowestY() }, 1.0f, color::Blue,
+		// -1.0f);
 	}
 };
 
+/*
 class MainMenu : public Scene {
 public:
 	Button play;
@@ -200,9 +219,10 @@ public:
 
 	void Update() override {}
 };
+*/
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
-	game.Init("One Bit Jam", window_size, color1);
+	game.Init("One Bit Jam", window_size, bg_color);
 	game.scene.Enter<GameScene>("game");
 	return 0;
 }
