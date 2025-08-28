dbebf207064e43a33f7608150386aebe22ef8aeb
Martin <emailregisterlogin@gmail.com>
2025-08-28 20:49:52 +0200

    Update application.cpp

--- brackeys_jam_2025_2/src/application.cpp
+++ brackeys_jam_2025_2/src/application.cpp
@@ -1,8 +1,10 @@
 #include "protegon/protegon.h"
+#include "renderer/api/origin.h"
 
 using namespace ptgn;
 
 constexpr V2_int resolution{ 320, 180 };
+constexpr V2_int world_size{ 480, 360 };
 
 void SetupWindow() {
 	game.window.SetSize(resolution * 4);
@@ -36,16 +38,36 @@ public:
 	Entity player;
 	Entity mouse;
 
+	void CreateHitbox(const V2_float& top_left, const V2_float& size) {
+		auto hitbox{ CreateEntity() };
+		SetPosition(hitbox, top_left);
+		SetDrawOrigin(hitbox, Origin::TopLeft);
+		hitbox.Add<Collider>(Rect{ size });
+	}
+
 	void Enter() override {
+		SetColliderVisibility(true);
+
 		SetupWindow();
 		SetBackgroundColor(color::LightBlue);
 
 		LoadResources("resources/resources.json");
 
-		/*CreateRect(*this, { 0, 0 }, { 30, 30 }, color::Red, -1.0f, Origin::TopLeft);
+		camera.SetBounds(-world_size / 2.0f, world_size);
+		physics.SetBounds(-world_size / 2.0f, world_size);
+
+		json walls = game.json.Get("walls");
+
+		for (json hitbox : walls.at("hitboxes")) {
+			CreateHitbox(hitbox.at("position").get<V2_float>(), hitbox.at("size").get<V2_float>());
+		}
+		CreateSprite(*this, "arena", { 0, 0 });
+
+		CreateRect(*this, -world_size / 2.0f, { 30, 30 }, color::Blue, -1.0f, Origin::TopLeft);
+		CreateRect(*this, { 0, 0 }, { 30, 30 }, color::Red, -1.0f, Origin::TopLeft);
 		CreateRect(*this, { resolution.x, 0 }, { 30, 30 }, color::Red, -1.0f, Origin::TopRight);
 		CreateRect(*this, resolution, { 30, 30 }, color::Red, -1.0f, Origin::BottomRight);
-		CreateRect(*this, { 0, resolution.y }, { 30, 30 }, color::Red, -1.0f, Origin::BottomLeft);*/
+		CreateRect(*this, { 0, resolution.y }, { 30, 30 }, color::Red, -1.0f, Origin::BottomLeft);
 
 		TopDownPlayerConfig player_config;
 		player_config.animation_frame_count = { 4, 3 };
