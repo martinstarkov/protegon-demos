e86dde3a579d0f4f0632c91e2a758b5b1c99770f
Martin <emailregisterlogin@gmail.com>
2025-08-28 22:20:45 +0200

    Update application.cpp

--- brackeys_jam_2025_2/src/application.cpp
+++ brackeys_jam_2025_2/src/application.cpp
@@ -1,3 +1,4 @@
+#include "math/geometry/circle.h"
 #include "protegon/protegon.h"
 #include "renderer/api/origin.h"
 
@@ -5,6 +6,12 @@ using namespace ptgn;
 
 constexpr V2_int resolution{ 320, 180 };
 constexpr V2_int world_size{ 480, 360 };
+
+constexpr CollisionCategory category_player{ 0 };
+constexpr CollisionCategory category_wall{ 1 };
+constexpr CollisionCategory category_enemy{ 2 };
+constexpr CollisionCategory category_enemy_projectile{ 3 };
+constexpr CollisionCategory category_player_projectile{ 4 };
 
 void SetupWindow() {
 	game.window.SetSize(resolution * 4);
@@ -20,12 +27,18 @@ struct FollowMouseScript : public Script<FollowMouseScript> {
 
 void CreateStraightBullet(Scene& scene, const V2_float& start_pos, const V2_float& dir_norm);
 
+struct BulletDisappearScript : public Script<BulletDisappearScript, CollisionScript> {
+	void OnCollision([[maybe_unused]] Collision collision) {
+		entity.Destroy();
+	}
+};
+
 struct ShootMouseBulletScript : public Script<ShootMouseBulletScript, GlobalMouseScript> {
 	void OnMouseDown(Mouse mouse_button) {
 		if (mouse_button == Mouse::Left) {
 			auto mouse{ GetChild(entity, "mouse") };
 			auto mouse_pos{ GetAbsolutePosition(mouse) };
-			auto entity_pos{ GetAbsolutePosition(entity) };
+			auto entity_pos{ GetAbsolutePosition(GetChild(entity, "body")) };
 			auto dir{ mouse_pos - entity_pos };
 			auto dir_norm{ dir.Normalized() };
 			CreateStraightBullet(entity.GetScene(), entity_pos, dir_norm);
@@ -40,9 +53,15 @@ public:
 
 	void CreateHitbox(const V2_float& top_left, const V2_float& size) {
 		auto hitbox{ CreateEntity() };
-		SetPosition(hitbox, top_left);
+		SetPosition(hitbox, top_left - world_size / 2.0f);
 		SetDrawOrigin(hitbox, Origin::TopLeft);
-		hitbox.Add<Collider>(Rect{ size });
+		auto& collider = hitbox.Add<Collider>(Rect{ size });
+		collider.SetCollisionCategory(category_wall);
+		collider.AddCollidesWith(category_player);
+		collider.AddCollidesWith(category_player_projectile);
+		collider.AddCollidesWith(category_enemy_projectile);
+		collider.AddCollidesWith(category_enemy);
+		hitbox.Add<RigidBody>().immovable = true;
 	}
 
 	void Enter() override {
@@ -73,11 +92,17 @@ public:
 		player_config.animation_frame_count = { 4, 3 };
 		player_config.animation_frame_size	= { 16, 17 };
 		player_config.animation_duration	= milliseconds{ 500 };
+		player_config.body_hitbox_offset	= { 0, 4 };
 
 		mouse = CreateSprite(*this, "cursor", {});
 		AddScript<FollowMouseScript>(mouse);
 
 		player = CreateTopDownPlayer(*this, { 0, 0 }, player_config);
+		auto& player_collider{ GetChild(player, "body").Get<Collider>() };
+		player_collider.SetCollisionCategory(category_player);
+		player_collider.AddCollidesWith(category_wall);
+		player_collider.AddCollidesWith(category_enemy);
+		// player.Get<RigidBody>().immovable = true;
 		AddScript<ShootMouseBulletScript>(player);
 
 		AddChild(player, mouse, "mouse");
@@ -92,14 +117,20 @@ public:
 };
 
 void CreateStraightBullet(Scene& scene, const V2_float& start_pos, const V2_float& dir_norm) {
-	auto bullet = CreateSprite(scene, "bullet1", start_pos);
-	auto& rb	= bullet.Add<RigidBody>();
+	Sprite bullet = CreateSprite(scene, "bullet1", start_pos);
+	auto& rb	  = bullet.Add<RigidBody>();
 	float bullet_speed{ 500.0f };
 	rb.velocity = dir_norm * bullet_speed;
 	bullet.Add<Lifetime>(milliseconds{ 1000 }, true);
 	float heading{ dir_norm.Angle() + DegToRad(90.0f) };
 	SetRotation(bullet, heading);
 	game.sound.Play("bullet1_sound");
+	auto& collider = bullet.Add<Collider>(Circle{ bullet.GetTextureSize().y / 2.0f });
+	collider.AddCollidesWith(category_wall);
+	collider.AddCollidesWith(category_enemy);
+	collider.SetCollisionCategory(category_player_projectile);
+	collider.response = CollisionResponse::Stick;
+	AddScript<BulletDisappearScript>(bullet);
 }
 
 int main() {
